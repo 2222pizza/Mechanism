6ca2dd511c6ae7fd62860064848a4d265359fd85
Donald Osborn <donaldosborn@gmail.com>
2022-10-26 16:12:35 -0400

    Character Controller Work (STARTING)
    Beginning work on 3rd-person character controller scripting
    
    Organization of various animations to utilize

---
+++ Mechanism/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float flightSpeed = 25.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        // TODO: else if the character is in air and flying
+
+        // Applying Gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Moving the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
