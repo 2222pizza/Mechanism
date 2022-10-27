8d8d77a4a3f4f1e063a60744f729b362873df789
Donald Osborn <donaldosborn@gmail.com>
2022-10-26 22:32:57 -0400

    [STARTED] Player Health System
    Health System intended for player usage. More work necessary to accommodate GUI implementation and playermodel death event.

---
+++ Mechanism/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image healthBar; //TODO: change this to adjust to UI implementation
+    public float healthAmount = 100;
+
+    private void Update()
+    {
+        //Player Death
+        if(healthAmount <= 0)
+        {
+            //TODO: Kill the player
+        }
+
+        //TODO: Take Damage triggers?
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        healthAmount -= Damage;
+
+        //Update Healthbar
+        healthBar.fillAmount = healthAmount / 100; //TODO: change this to accomodate GUI implementation
+    }
+
+    public void RestoreHealth(float HealthPoints)
+    {
+        healthAmount += HealthPoints;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100); //Prevent HealthBar from going outside bounds of 0 and 100
+
+        //Update Healthbar
+        healthBar.fillAmount = healthAmount / 100; //TODO: change this to accomodate GUI implementation
+    }
+}
