4942e094da065bcc5cf33392a362c7fbfc3d2fd8
Donald Osborn <donaldosborn@gmail.com>
2022-10-31 09:16:30 -0400

    Animation Progress
    Reorganized Input mapping, animation managing into assets/scripts/
    
    Implementation of input mapping, animation manager, player manager/controller

---
+++ Mechanism/Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+    AnimatorManager animatorManager;
+
+    public Vector2 movementInput;
+    private float moveAmount;
+
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovement();
+        //TODO: HandleJump();
+        //TODO: HandleAttack();
+        //TODO: HandleFlight();
+    }
+
+    private void HandleMovement()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
+    }
+
+}
---
+++ Mechanism/Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+
+    public InputManager inputManager;
+
+    public Vector3 moveDirection;
+    public Transform cameraObject;
+    public Rigidbody playerRigidbody;
+
+    //Change player speed by changing this variable
+    public float movementSpeed = 7;
+    public float rotationSpeed = 50;
+
+    private void Awake(){
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement(){
+        //MOVEMENT INPUT
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection = moveDirection * movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+}
